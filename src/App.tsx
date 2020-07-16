fa207222c564ddfc39337bfb460e8ecf51f83792
Subha Sarkar <sarkarsubha1206@gmail.com>
2020-07-16 16:31:03 +0530

    master branch commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import Search from './components/search'
+
 
 function App() {
   return (
     <div className="App">
-     <Search name="ddsds"/>
+     
     </div>
   );
 }
